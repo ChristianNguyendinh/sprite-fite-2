af4aa024596523c10a3242c8f3e5c1027cc1b188
Christian <christiannguyendinh@yahoo.com>
2017-01-22 16:08:22 -0500

    Character Select ES6 conversion

--- cards.js
+++ cards.js
@@ -1,4 +1,4 @@
-{
+export default {
 	"cards":[
 		{
 			"name" : "AverageJoe", 
--- public/js/components/CharSel_rework.js
+++ public/js/components/CharSel_rework.js
@@ -1,25 +1,20 @@
-// LETS CONVERT THIS TO ES6 NOW!
-
-var React = require('react');
-var cardsJSON = require('../../../cards.json');
+import React from 'react'
+import cardsJSON from '../../../cards.js'
 
 // These will live on the server eventually. Leave them as global vars for now for ease of testing
 var p1Cards = [];
 var p2Cards = [];
 
-var CharacterTile = React.createClass({
-	propTypes: {
-		card: React.PropTypes.object,
-		savec: React.PropTypes.func,
-		unsavec: React.PropTypes.func
-	},
-	getInitialState: function() {
-		return {
-			isSelected: false,
+class CharacterTile extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            isSelected: false,
 			selectedGlow: {}
-		};
-	},
-	handleClick: function(e) {
+        }
+    }
+
+	handleClick(e) {
 		if (!this.state.isSelected) {
 			// Don't add if each player has already picked three cards
 			if (p1Cards.length != 3 || p2Cards.length != 3) {
@@ -27,27 +22,29 @@ var CharacterTile = React.createClass({
 				this.setState({
 					isSelected: true,
 					selectedGlow: {boxShadow: "0px 0px 30px #0f0"}
-				}, this.sendCard);
+				}, this.sendCard.bind(this));
 			}
 		} else {
-			console.log(this.props.card.name + " UNSELECTED.");
+			console.log(this.props.card.name + " UNSELECTED!!!.");
 			this.setState({
 				isSelected: false,
 				selectedGlow: {}
-			}, this.sendCard);
+			}, this.sendCard.bind(this));
 		}
-	},
-	sendCard: function() {
+	}
+
+	sendCard() {
 		let player = p1Cards.length != 3 ? "p1" : "p2";
 		if (this.state.isSelected) {
 			this.props.savec(this.props.card, player);
 		} else {
 			this.props.unsavec(this.props.card, player);
 		}
-	},
-	render: function() {
+	}
+
+	render() {
 		return (
-			<div className="characterTile" onClick={this.handleClick} style={this.state.selectedGlow}>
+			<div className="characterTile" onClick={this.handleClick.bind(this)} style={this.state.selectedGlow}>
 			<center>
 				<p>{this.props.card.name}</p>
 				<img src={"images/" + this.props.card.image} />
@@ -55,13 +52,17 @@ var CharacterTile = React.createClass({
 			</div>
 		);
 	}
-});
+}
 
-var CharacterSelect = React.createClass({
-	propTypes: {
-		changeGameState: React.PropTypes.func
-	},
-	saveCard: function(card, player) {
+CharacterTile.propTypes = {
+    card: React.PropTypes.object,
+    savec: React.PropTypes.func,
+    unsavec: React.PropTypes.func
+};
+
+class CharacterSelect extends React.Component {
+
+	saveCard(card, player) {
 		let arr = player == "p1" ? p1Cards : p2Cards;
 		// Copy stats over to new card to use for battle
 		let newCard = {
@@ -73,8 +74,9 @@ var CharacterSelect = React.createClass({
 			"dead": false
 		};
 		arr.push(newCard);
-	},
-	unsaveCard: function(card) {
+	}
+
+	unsaveCard(card) {
 		// Remove the card from the player's selected card pile
 		for (let i = 0; i < p1Cards.length; i++) {
 			if (p1Cards[i].name == card.name) {
@@ -88,16 +90,18 @@ var CharacterSelect = React.createClass({
 				return;
 			}
 		}
-	},
-	handleClick: function() {
+	}
+
+	handleClick(e) {
 		if (p1Cards.length == 3 && p2Cards.length == 3) {
 			this.props.changeGameState("game");
 		} else {
 			// temp error message
 			console.error("Each player needs to pick 3 cards!");
 		}
-	},
-	render: function() {
+	}
+
+	render() {
 		return (
 			<div style={{backgroundColor: "blue", width: "100%", height: "100%"}}>
 				<h1>Cards</h1>
@@ -112,35 +116,39 @@ var CharacterSelect = React.createClass({
 				<div>
 					<h3>P2 Cards</h3>
 				</div>
-				<button onClick={this.handleClick}> GO </button>
+				<button onClick={this.handleClick.bind(this)}> GO </button>
 			</div>
 		);
 	}
-});
+}
+
+CharacterSelect.propTypes = {
+    changeGameState: React.PropTypes.func
+};
 
 
 ///////////////////////////////////////////////
 
-
-var Rework = React.createClass({
-	getInitialState: function() {
-		return {
+class Rework extends React.Component {
+	constructor(props) {
+		super(props);
+		this.state = {
 			// pick, game, p1win, or p2win
 			gameState: "pick"
-		};
-	},
-	changeGameState: function(newState) {
+		}
+	}
+
+	changeGameState(newState) {
 		console.log("Chaning game state to: " + newState);
 		this.setState({
 			gameState: newState
 		});
-	},
-	render: function() {
-		var content
+	}
+
+	render() {
+		var content;
 		if (this.state.gameState == "pick")
-			content = <CharacterSelect changeGameState={this.changeGameState}/>;
-		else if (this.state.gameState == "game")
-			content = <GameBoard changeGameState={this.changeGameState}/>;
+			content = <CharacterSelect changeGameState={this.changeGameState.bind(this)}/>;
 		else
 			content = null;
 
@@ -150,6 +158,6 @@ var Rework = React.createClass({
 			</div>
 		);
 	}
-});
+}
 
 module.exports = Rework;
